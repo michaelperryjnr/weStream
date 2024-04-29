03f174375bc1053b0d41dab4d4f40793ac159982
michaelperryjnr <michaelperryt97@gmail.com>
2024-04-29 16:21:12 +0000

    Reset file

--- server/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-import Chat from "./chats";
-import Message from "./messages";
-import Session from "./session";
-import User from "./user";
-import UserChat from "./userChats";
-import { UserChatArray } from "./chats";
-
-export type { Chat, Message, Session, User, UserChat, UserChatArray };
--- server/interfaces/messages.ts
+++
@@ -1,9 +0,0 @@
-export default interface Message {
-  id: string;
-  senderId: string;
-  message: string;
-  date: null;
-  img: string;
-  lastEdited: null;
-  isEdited: boolean;
-}
