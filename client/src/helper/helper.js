18647cfdd483b6bdf4b5d54ac99dee118ff8ad61
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-03 16:54:25 +0000

    Added function to get discovery

---
+++ client/src/helper/helper.js
@@ -0,0 +1,28 @@
+/**fetch discover movie */
+import React from "react";
+import { API_KEY } from "./config";
+
+const url =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: API_KEY,
+  },
+};
+
+
+const getDiscover = async () =>  {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+export default getDiscover;
