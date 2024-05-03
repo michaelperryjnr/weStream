115dd0103aed119d166be310bc23194295427222
michaelperryjnr <michaelperryt97@gmail.com>
2024-05-03 22:29:40 +0000

    Added function to get popular movies

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,9 +1,12 @@
 /**fetch discover movie */
-import React from "react";
+// import React from "react";
 import { API_KEY } from "./config";
 
-const url =
+const discoveryUrl =
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+
+const popularUrl =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
 const options = {
   method: "GET",
@@ -13,18 +16,24 @@ const options = {
   },
 };
 
-
-const getDiscover = async () =>  {
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error.message)
-    }
-}
-
-
-export default getDiscover;
+export async function getDiscover() {
+  try {
+    const response = await fetch(discoveryUrl, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 //change to API key
+export async function getPopular ()  {
+  try {
+    const response = await fetch(popularUrl, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
